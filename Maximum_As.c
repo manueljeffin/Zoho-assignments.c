45e5f2be12d0f9a662a30833dc10e8a0246b2129
Karthik-Ragunath <a.karthikragunath@gmail.com>
2016-04-07 08:35:45 +0530

    Create Maximum_As.c

---
+++ Maximum_As.c
@@ -0,0 +1,57 @@
+# Zoho-assignments.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int testcases,k;
+    printf("Enter the number of testcases :\n");
+    
+    scanf("%d",&testcases);
+    for(k=0;k<testcases;k++)
+    {
+        int keystrokes;
+        int solution=0;
+        int finisher;
+        printf("Enter the number of keystrokes :\n");
+        
+        scanf("%d",&keystrokes);
+        
+        while(keystrokes>0)
+        {
+            if((keystrokes>3)&&(solution<3))
+            {
+                finisher=solution;
+                solution=3;
+                keystrokes=keystrokes-3;
+                continue;
+            }
+            if((solution<3)&&(keystrokes<=3))
+            {
+                solution=keystrokes;
+                break;
+            }
+            if((solution>=3)&&(keystrokes>=4))
+            {
+                finisher=solution;
+                solution=solution+solution*2;
+                keystrokes=keystrokes-4;
+                continue;
+            }
+            if((solution>3)&&(keystrokes<4))
+            {
+                solution=solution+finisher*keystrokes;
+                break;
+            }
+            if((solution==3)&&(keystrokes<4))
+            {
+                solution=solution+keystrokes;
+                break;
+            }
+        }
+        
+        //------------SOLUTION---------------------------
+        printf("The maximum number of A's that can be printed using the given strokes is : %d\n",solution);
+    }
+    return 0;
+}
